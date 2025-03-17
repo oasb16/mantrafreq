06456fb36800990da1deedae95caa22f45c4fb88
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-03-16 17:26:10 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import numpy as np
 from scipy.fft import fft
